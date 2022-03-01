631573ced2fa027c72a15bfe8aeba1ab6dc8e52e
dalj <diegoa.londono@udea.edu.co>
2022-02-28 22:56:00 -0500

    En el código Trasmisor se lleva al void setup parte del codigo del void loop para mirar solamente un ciclo y comprobar que está funcionando.
    Se comprueba la clave y la lógica combinacional responde exitosamente

--- Conexión entre 2 arduinos/Codigo fuente arduino receptor.ino
+++ Conexión entre 2 arduinos/Codigo fuente arduino receptor.ino
@@ -1,26 +1,72 @@
+/*
+  LiquidCrystal Library - Hello World
+
+ Demonstrates the use a 16x2 LCD display.  The LiquidCrystal
+ library works with all LCD displays that are compatible with the
+ Hitachi HD44780 driver. There are many of them out there, and you
+ can usually tell them by the 16-pin interface.
+
+ This sketch prints "Hello World!" to the LCD
+ and shows the time.
+
+  The circuit:
+ * LCD RS pin to digital pin 12
+ * LCD Enable pin to digital pin 11
+ * LCD D4 pin to digital pin 5
+ * LCD D5 pin to digital pin 4
+ * LCD D6 pin to digital pin 3
+ * LCD D7 pin to digital pin 2
+ * LCD R/W pin to ground
+ * LCD VSS pin to ground
+ * LCD VCC pin to 5V
+ * 10K resistor:
+ * ends to +5V and ground
+ * wiper to LCD VO pin (pin 3)
+
+ Library originally added 18 Apr 2008
+ by David A. Mellis
+ library modified 5 Jul 2009
+ by Limor Fried (http://www.ladyada.net)
+ example added 9 Jul 2009
+ by Tom Igoe
+ modified 22 Nov 2010
+ by Tom Igoe
+
+ This example code is in the public domain.
+
+ http://www.arduino.cc/en/Tutorial/LiquidCrystal
+ */
 //ARDUINO RECEPTOR
+// include the library code:
+#include <LiquidCrystal.h>
 #define reloj 13
 #define banderaRX 6
-#define infoEncriptada 3
+#define infoEncriptada 7
 int8_t i=0;
 int val=0;
 
-void setup()
-{
+// initialize the library with the numbers of the interface pins
+LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
+
+void setup() {
+  // set up the LCD's number of columns and rows:
+  lcd.begin(16, 2);
   Serial.begin(9600);
   pinMode(reloj, INPUT); ////Pin para la señal de reloj
   pinMode(infoEncriptada, INPUT);// Pin para recibir la información "encriptada"
   pinMode(banderaRX, OUTPUT); //Bandera de RX. Se enciende cuando logra leer el bit
-
 }
 
-void loop(){
-  int infoRX[10] = {8,0,7,4,0,0,0,0,0,0};
+void loop() {
+  
+  int infoRX[10] = {};
   
     if(digitalRead(reloj)){  //Si detecta flanco de subida:
       val= digitalRead(infoEncriptada); //Lee la información
       infoRX[i]= val; //Va guardando la información en un array
-      i++;
+      lcd.setCursor(i, 0); //Se ubica el cursor en la columna i, fila 0
+      lcd.print(infoRX[i]);
+      i++;      
       digitalWrite(banderaRX,1); //Se enciende cuando logra recibir el bit
       delay(20);
     } 
@@ -29,7 +75,10 @@ void loop(){
       
       Serial.print("RX ");
       Serial.println(val);
+  
   if(i==10){
     i=0;
-  }
+    lcd.clear();
+  }  
 }
+ 
--- Conexión entre 2 arduinos/Codigo fuente arduino transmisor.ino
+++ Conexión entre 2 arduinos/Codigo fuente arduino transmisor.ino
@@ -1,8 +1,22 @@
-﻿//Puede verse en https://www.tinkercad.com/things/3oNF9EBDzeW
-//ARDUINO TRANSMISOR
-#define reloj 13 
+﻿//ARDUINO TRANSMISOR
+#define reloj 13
 #define banderaRX 6
 #define infoENC 3
+//#define SRCLK 9   //pin 9 a SRCLK del 74HC595
+
+int8_t pinRCLK = 10; //Pin del registro de salida. AMARILLO
+int8_t pinSRCLK = 9; //Pin del registro de desplazamiento. ROSADO
+
+void OnSRCLK() { //Función para activar el reloj de la etapa de desplazamiento
+  digitalWrite(pinSRCLK, LOW);
+  digitalWrite(pinSRCLK, HIGH);
+  digitalWrite(pinSRCLK, LOW);
+}
+void OnRCLK() { //Función para activar el reloj de la etapa de salida.
+  digitalWrite(pinRCLK, LOW);
+  digitalWrite(pinRCLK, HIGH);
+  digitalWrite(pinRCLK, LOW);
+}
 
 int val=0;
 
@@ -12,15 +26,15 @@ void setup()
   pinMode(reloj, OUTPUT); //Pin para la señal de reloj
   pinMode(infoENC, OUTPUT); // Pin para enviar la información "encriptada"
   pinMode(banderaRX, INPUT);  //Bandera de RX indicando que recibió
-}
-
-void loop()
-{
-  int informacion[10] = {1,0,0,1,1,0,1,0,1,1}; //Información "encriptada"
+int informacion[10] = {1,0,0,0,1,1,1,0,1,0}; //Información "encriptada"
   
   for(int8_t i=0; i<10; i++){
+    
     digitalWrite(reloj, HIGH); //Flanco de subida señal de reloj
+    //shiftOut(infoENC, SRCLK, MSBFIRST, informacion[i]);
     digitalWrite(infoENC,informacion[i]); //Se envía bit a bit 
+    OnSRCLK();
+    OnRCLK();
     while(digitalRead(banderaRX)==LOW){ //Si el RX no ha podido leer : espere
           Serial.print("esperando");
     } 
@@ -29,4 +43,27 @@ void loop()
     Serial.print("El valor enviado es");
     Serial.println(val);
   } 
+
+
+}
+
+void loop()
+{
+ /* int informacion[10] = {1,0,0,1,1,0,1,0,1,1}; //Información "encriptada"
+  
+  for(int8_t i=0; i<10; i++){
+    
+    digitalWrite(reloj, HIGH); //Flanco de subida señal de reloj
+    //shiftOut(infoENC, SRCLK, MSBFIRST, informacion[i]);
+    digitalWrite(infoENC,informacion[i]); //Se envía bit a bit 
+    OnSRCLK();
+    OnRCLK();
+    while(digitalRead(banderaRX)==LOW){ //Si el RX no ha podido leer : espere
+          Serial.print("esperando");
+    } 
+    digitalWrite(reloj, LOW); //Flanco de bajada señal de reloj
+
+    Serial.print("El valor enviado es");
+    Serial.println(val);
+  } */
 }
