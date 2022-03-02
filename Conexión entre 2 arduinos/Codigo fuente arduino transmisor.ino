ad24cda541bcbd517301fab77b4bce5f8b3da383
dalj <diegoa.londono@udea.edu.co>
2022-03-01 21:29:01 -0500

    Se hacen las pruebas enviando hasta tres enteros de 8 bits, se agrega la funcion para encontrar el tamaño del arreglo que entregará el profesor y así saber hasta donde se recorre el arreglo

--- Conexión entre 2 arduinos/Codigo fuente arduino transmisor.ino
+++ Conexión entre 2 arduinos/Codigo fuente arduino transmisor.ino
@@ -2,7 +2,7 @@
 #define reloj 13
 #define banderaRX 6
 #define infoENC 3
-//#define SRCLK 9		//pin 9 a SRCLK del 74HC595
+//#define SRCLK 9    //pin 9 a SRCLK del 74HC595
 
 int8_t pinRCLK = 10; //Pin del registro de salida. AMARILLO
 int8_t pinSRCLK = 9; //Pin del registro de desplazamiento. ROSADO
@@ -18,78 +18,65 @@ void OnRCLK() { //Función para activar el reloj de la etapa de salida.
   digitalWrite(pinRCLK, LOW);
 }
 
-int val=0;
-
+int val = 0;
+int k = 0;
 void setup()
 {
   Serial.begin(9600);
   pinMode(reloj, OUTPUT); //Pin para la señal de reloj
   pinMode(infoENC, OUTPUT); // Pin para enviar la información "encriptada"
   pinMode(banderaRX, INPUT);  //Bandera de RX indicando que recibió
-  
-//int informacion[10] = {1,0,0,0,1,1,1,0,1,0}; //Información "encriptada"
-  
-  int informacion[8] = {0}; 
-  int info[1] = {58}; //Información "encriptada"
-  
-  ///INICIO QT
-    short binario[8]={0};
-        
-    
-  for (int j=0;j<1;j++){
-    
-    for (int i=0;i<8 ;i++ ) {
-        informacion[i]= info[j] % 2;
-        info[j] /= 2;
-    }
-  }
-
-    //for (int i = 7;i>=0 ;i-- ) {
-   //     cout << binario[i];
-    //}
- 
-
-////FIN QT
- 
-  
-
-  Serial.print("---------");
-  for(int8_t i=7; i>=0; i--){
-
-    digitalWrite(reloj, HIGH); //Flanco de subida señal de reloj
-    //shiftOut(infoENC, SRCLK, MSBFIRST, informacion[i]);
-    digitalWrite(infoENC,informacion[i]); //Se envía bit a bit 
-    OnSRCLK();
-    OnRCLK();
-    while(digitalRead(banderaRX)==LOW){ //Si el RX no ha podido leer : espere
-          Serial.print("esperando");
-    } 
-    digitalWrite(reloj, LOW); //Flanco de bajada señal de reloj
-
-    Serial.print("El valor enviado es");
-    Serial.println(val);
-  } 
-
 
 }
 
 void loop()
 {
- /* int informacion[10] = {1,0,0,1,1,0,1,0,1,1}; //Información "encriptada"
-  
-  for(int8_t i=0; i<10; i++){
-    
-    digitalWrite(reloj, HIGH); //Flanco de subida señal de reloj
-    //shiftOut(infoENC, SRCLK, MSBFIRST, informacion[i]);
-    digitalWrite(infoENC,informacion[i]); //Se envía bit a bit 
-    OnSRCLK();
-    OnRCLK();
-    while(digitalRead(banderaRX)==LOW){ //Si el RX no ha podido leer : espere
-          Serial.print("esperando");
-    } 
-    digitalWrite(reloj, LOW); //Flanco de bajada señal de reloj
 
-    Serial.print("El valor enviado es");
-    Serial.println(val);
-  } */
-}
+  Serial.println("NUEVO VOID LOOP");
+  Serial.println(k);
+
+  while (k < 1) {
+
+
+    byte informacion[8] = {0};
+    int info[] = {117,23,58}; //Información "encriptada"
+
+    int size = sizeof(info) / sizeof(int);
+    
+    
+    for (int j = 0; j < size; j++) {
+
+      for (int i = 0; i < 8 ; i++ ) {
+        informacion[i] = info[j] % 2;
+        info[j] /= 2;
+      }
+
+
+      Serial.print("---------");
+
+      for (int8_t i = 7; i >= 0; i--) {//se envian datos al SER
+
+        digitalWrite(reloj, HIGH); //Flanco de subida señal de reloj
+        //shiftOut(infoENC, SRCLK, MSBFIRST, informacion[i]);
+        digitalWrite(infoENC, informacion[i]); //Se envía bit a bit
+        OnSRCLK();
+        OnRCLK();
+        while (digitalRead(banderaRX) == LOW) { //Si el RX no ha podido leer : espere
+          Serial.println("esperando");
+        }
+        digitalWrite(reloj, LOW); //Flanco de bajada señal de reloj
+
+        Serial.print("El valor enviado es ");
+        Serial.println(informacion[i]);
+      }//for SER
+
+
+
+    }//fin for j
+
+    k++;
+
+  }//fin while
+
+  
+}//fin loop
